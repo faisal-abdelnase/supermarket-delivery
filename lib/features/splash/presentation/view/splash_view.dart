b68b45da3780e5208d4bc54e1bd9fdb8a195f564
faisal <faisalabdalnasser275@gmail.com>
2025-02-23 01:25:49 +0200

    create splash screen

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:super_market_app/constant.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>  with SingleTickerProviderStateMixin {
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    initSplashAnimation();
+    // navigateToHome();
+    super.initState();
+  }
+
+  
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: [
+            Image.asset(logo,width: 200,),
+            const SizedBox(height: 20,),
+            AnimatedBuilder(
+              animation: slidingAnimation,
+              builder: (context, _){
+                return SlideTransition(
+                  position: slidingAnimation,
+                  child: Text("Super Market Delivery", 
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Lobster"
+              ),),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  void initSplashAnimation(){
+    animationController = AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0,10), end: Offset.zero).animate(animationController);
+      animationController.forward();
+    
+  }
+
+  void navigateToHome() {
+    Future.delayed(Duration(seconds: 3),(){
+        Navigator.pushReplacementNamed(context, "/homeView");
+      });
+  }
+
+
+}
