11dffc30565a431150877f6052f5c20309932e80
faisal <faisalabdalnasser275@gmail.com>
2025-02-28 21:37:47 +0200

    create model to about app

---
+++ lib/features/about/data/model/about_data_model.dart
@@ -0,0 +1,8 @@
+class AboutDataModel{
+
+  final String image;
+  final String title;
+  final String subTitle;
+
+  AboutDataModel({required this.image, required this.title, required this.subTitle});
+}
