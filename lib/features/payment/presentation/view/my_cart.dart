878fbda25ab2770b710abca59c7c85b30c6a4e5c
faisal <faisalabdalnasser275@gmail.com>
2025-04-16 23:01:19 +0200

    add button in cart page to check out

--- lib/features/payment/presentation/view/my_cart.dart
+++ lib/features/payment/presentation/view/my_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:super_market_app/core/utils/widgets/custom_arrow_back_button.dart';
+import 'package:super_market_app/core/utils/widgets/custom_elvated_button.dart';
 import 'package:super_market_app/features/payment/presentation/view/widgets/cart_item.dart';
 import 'package:super_market_app/features/payment/presentation/view/widgets/coupon_code_text_field.dart';
 import 'package:super_market_app/features/payment/presentation/view/widgets/reset_cart.dart';
@@ -10,7 +11,8 @@ class MyCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         top: true,
@@ -43,6 +45,12 @@ class MyCart extends StatelessWidget {
                 CouponCodeTextField(),
 
                 ResetCart(),
+                
+                CustomElvatedButton(
+                  screenWidth: screenWidth, 
+                  screenHeight: screenHeight, 
+                  text: "Check Out", 
+                  onPressed: (){}),
               ],
             ),
           ),
